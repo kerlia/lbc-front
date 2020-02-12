1e2c1afde84295c6d066ca8092346824abb732e4
Guillaume B <guillaume@kura.fr>
2020-02-12 17:16:59 +0100

    Set search bar

--- src/containers/Offers.js
+++ src/containers/Offers.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Offers() {
   return (
-    <div>
-      <h1>Component Offers</h1>
-    </div>
+    <>
+      <div className="ellipse"></div>
+      <main className="main container">
+        <div className="search-bar">
+          <form action="">
+            <span>
+              <FontAwesomeIcon className="icon" icon="search" />
+              <input
+                placeholder="Que recherchez-vous?"
+                type="text"
+                name="query"
+                value=""
+              />
+            </span>
+            <input type="submit" value="Rechercher" />
+          </form>
+        </div>
+      </main>
+    </>
   );
 }
 
