9b17d116546201674d2781c66d42315ebf85cf39
Guillaume B <guillaume@kura.fr>
2020-02-12 14:14:33 +0100

    Arch project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React from "react";
-
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Offers from "./containers/Offers";
+import React, { useState, useEffect } from "react";
 
 import "./css/reset.css";
 import "./css/App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
+
+import Offers from "./containers/Offers";
+import Offer from "./containers/Offer";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
+library.add(faEnvelope, faKey);
+
 function App() {
+  const API = "https://leboncoin-api.herokuapp.com/api/offer/with-count";
+  const [count, setCount] = useState(0);
+  const [offers, setOffers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API);
+      setCount(response.data.count);
+      setOffers(response.data.offers);
+      setIsLoading(false);
+    } catch (e) {
+      console.error("Error fetching data from api");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // console.log("Count", count);
+  // console.log("Offers", offers);
+
   return (
-    <div>
-      <div>
-        <h1>Titre de mon site</h1>
-      </div>
-      <Router>
-        <Switch>
-          <Route path="/offers">
-            <Offers />
-          </Route>
-          {/* <Route path="/">
-            <Home />
-          </Route> */}
-        </Switch>
-      </Router>
-    </div>
+    <>
+      {isLoading ? (
+        <p>...Loading</p>
+      ) : (
+        <div>
+          <div>
+            <h1>Le Bon Coin</h1>
+          </div>
+          <Router>
+            <Switch>
+              <Route path="/offer">
+                <Offer />
+              </Route>
+              <Route path="/">
+                <Offers />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
+    </>
   );
 }
 
