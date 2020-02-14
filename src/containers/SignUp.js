2a6a533915da84d96247c9c24dd5b92a6d826b59
Guillaume B <guillaume@kura.fr>
2020-02-14 16:38:15 +0100

    Set final signup layout

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import Cookies from "js-cookie";
 import axios from "axios";
 
@@ -26,6 +26,7 @@ function SignUp({ user, setUser }) {
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
+  const [cgv, setCgv] = useState(false);
 
   const handlePseudoChange = e => {
     const value = e.target.value;
@@ -43,6 +44,10 @@ function SignUp({ user, setUser }) {
     const value = e.target.value;
     setPassword2(value);
   };
+  const handleCGVChange = e => {
+    const checked = e.target.checked;
+    setCgv(checked);
+  };
   const handleSubmit = async e => {
     e.preventDefault();
     console.log(pseudo, email, password1);
@@ -51,6 +56,7 @@ function SignUp({ user, setUser }) {
       alert("please fill in all fields");
     } else if (password1 !== password2) {
       alert("The two passwords are not identical");
+    } else if (cgv !== true) {
     } else {
       console.log(" >>>> SUBMIT");
       const newUser = {
@@ -60,6 +66,7 @@ function SignUp({ user, setUser }) {
       };
       const response = await axios.post(API, newUser);
       Cookies.set("lbc-cook", response.data.token);
+      setUser({ "lbc-cook": response.data.token });
       history.push("/");
       console.log(response.data);
     }
@@ -71,36 +78,34 @@ function SignUp({ user, setUser }) {
         <div className="col-left">
           <h2>Pourquoi créer un compte</h2>
           <div>
+            <FontAwesomeIcon className="icon" icon="clock" />
             <div>
-              <FontAwesomeIcon className="icon" icon="clock" />
-              <div>
-                <h3>Gagnez du temps</h3>
-                <p>
-                  Publiez vos annonces rapidement, avec vos informations
-                  pré-remplies chaque fois que vous souhaitez déposer une
-                  nouvelle annonce.
-                </p>
-              </div>
+              <h3>Gagnez du temps</h3>
+              <p>
+                Publiez vos annonces rapidement, avec vos informations
+                pré-remplies chaque fois que vous souhaitez déposer une nouvelle
+                annonce.
+              </p>
             </div>
+          </div>
+          <div>
+            <FontAwesomeIcon className="icon" icon="bell" />
             <div>
-              <FontAwesomeIcon className="icon" icon="bell" />
-              <div>
-                <h3>Soyez les premiers informés</h3>
-                <p>
-                  Créez des alertes Immo ou Emploi et ne manquez jamais
-                  l’annonce qui vous intéresse.
-                </p>
-              </div>
+              <h3>Soyez les premiers informés</h3>
+              <p>
+                Créez des alertes Immo ou Emploi et ne manquez jamais l’annonce
+                qui vous intéresse.
+              </p>
             </div>
+          </div>
+          <div>
+            <FontAwesomeIcon className="icon" icon="eye" />
             <div>
-              <FontAwesomeIcon className="icon" icon="eye" />
-              <div>
-                <h3>Visibilité</h3>
-                <p>
-                  Suivez les statistiques de vos annonces (nombre de fois où
-                  votre annonce a été vue, nombre de contacts reçus)
-                </p>
-              </div>
+              <h3>Visibilité</h3>
+              <p>
+                Suivez les statistiques de vos annonces (nombre de fois où votre
+                annonce a été vue, nombre de contacts reçus)
+              </p>
             </div>
           </div>
         </div>
@@ -125,24 +130,46 @@ function SignUp({ user, setUser }) {
               defaultValue=""
               onChange={handleEmailChange}
             />
-            <label htmlFor="form-password1">Mot de passe *</label>
-            <input
-              placeholder="**********"
-              type="password"
-              name="password1"
-              id="form-password1"
-              defaultValue=""
-              onChange={handlePassword1Change}
-            />
-            <label htmlFor="form-password2">Confirmer le mot de passe *</label>
-            <input
-              placeholder="**********"
-              type="password"
-              name="password2"
-              id="form-password2"
-              defaultValue=""
-              onChange={handlePassword2Change}
-            />
+            <div>
+              <div>
+                <label htmlFor="form-password1">Mot de passe *</label>
+                <input
+                  placeholder="**********"
+                  type="password"
+                  name="password1"
+                  id="form-password1"
+                  defaultValue=""
+                  onChange={handlePassword1Change}
+                />
+              </div>
+              <div>
+                <label htmlFor="form-password2">
+                  Confirmer le mot de passe *
+                </label>
+                <input
+                  placeholder="**********"
+                  type="password"
+                  name="password2"
+                  id="form-password2"
+                  defaultValue=""
+                  onChange={handlePassword2Change}
+                />
+              </div>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="form-cgv"
+                name="cgv"
+                onChange={handleCGVChange}
+              />
+              <label htmlFor="form-cgv" class="cgv">
+                « J’accepte les{" "}
+                <Link to="#">Conditions Générales de Vente</Link>
+                &nbsp;et les{" "}
+                <Link to="#">Conditions Générales d’Utilisation</Link> »
+              </label>
+            </div>
             <input
               type="submit"
               value="Créer mon Compte Personnel"
